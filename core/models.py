98c5839ab259403eca1eb98498e2c2e33d03dc15
Àngel Arreola <angelarreola.cheesecake@gmail.com>
2026-02-18 20:56:38 -0600

    ✨ feat: add initial migration for RawBlogsEntries model and update .env.example with new config vars

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
+import uuid
 from django.db import models
 
+
 # Create your models here.
+class RawBlogsEntries(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    main_image_url = models.TextField()
+    title = models.TextField()
+    subtitle = models.TextField()
+    info_subtitles = models.JSONField()
+    info_data = models.TextField()
