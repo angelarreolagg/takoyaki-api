aa25e803211443d4dfdadbf4c59dcbb0a2c344a3
Àngel Arreola <angelarreola.cheesecake@gmail.com>
2026-02-20 19:37:06 -0600

    ✨ feat: add celery configuration (settings.py important to modify in order to work properly)

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
+from news.views import NewsViewSet
 
 app_name = 'api'
 
+router = DefaultRouter()
+router.register(r'news', NewsViewSet, basename='news')
+
 urlpatterns = [
     path('status/', views.status_check, name='status'),
+    path('', include(router.urls)),
 ]
--- core/tasks.py
+++ core/tasks.py
@@ -1,5 +1,39 @@
 from celery import shared_task
+from .services.scraper.scraper_service import scrape_news
+from .services.ai.ai_service import rewrite_entry_with_ai
+from .serializers import BlogProcessedEntriesSerializer
+
 
 @shared_task
-def add(x, y):
-    return x + y
+def info_source_scraper():
+    print("--- Initializing Scraper ---")
+    # Scraping service
+    raw_scraped_data = scrape_news()
+    # Process raw data with IA
+    for entry in raw_scraped_data:
+        success = process_entry(entry)
+        if not success:
+            print(f"Skipping entry due to error: {entry.get('title')}")
+    return f"{len(raw_scraped_data)} entries processed sequentially."
+
+
+def process_entry(entry):
+    data_for_ai = entry.copy()
+    data_for_ai.pop('main_image_url', None)
+    try:
+        print(f"processing with AI [{data_for_ai.get('title')}]")
+        processed = rewrite_entry_with_ai(data_for_ai)
+        if processed:
+            processed['main_image_url'] = entry.get('main_image_url')
+            print(f"SUCCESS PROCESS: {processed['title']}")
+            data_to_save = {"data": processed}
+            serializer = BlogProcessedEntriesSerializer(data=data_to_save)
+            if serializer.is_valid():
+                serializer.save()
+                print(f"SUCCESS SAVING: {processed['title']}")
+            else:
+                print(f"Error saving entry {entry.get('title')}: {serializer.errors}")
+            return True
+    except Exception as e:
+        print(f"Error processing {entry.get('title')}: {e}")
+        return False
